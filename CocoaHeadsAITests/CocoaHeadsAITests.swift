7c3af0b87f3e0465cf55d02f173936539ef45e32
Dimitri Brukakis <dimitri@brukakis.net>
2025-10-03 10:45:57 +0200

    Initial Commit

---
+++ CocoaHeadsAITests/CocoaHeadsAITests.swift
@@ -0,0 +1,16 @@
+//
+//  CocoaHeadsAITests.swift
+//  CocoaHeadsAITests
+//
+//  Created by Dimitri Brukakis on 03.10.25.
+//
+
+import Testing
+
+struct CocoaHeadsAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
